b12de25685dbcba9ac41e442cdc68bcf77d23a1d
Joao Vidigal <j.lucasvidigal@gmail.com>
2026-07-01 01:34:28 -0400

    remove comments

--- test_temperatura.py
+++ test_temperatura.py
@@ -4,12 +4,10 @@ from temperatura import fahrenheit_para_celsius, celsius_para_fahrenheit
 class TestTemperatura(unittest.TestCase):
     
     def test_fahrenheit_para_celsius(self):
-        # Testa o ponto de congelamento da água
         self.assertEqual(fahrenheit_para_celsius(32), 0)
         
     def test_celsius_para_fahrenheit(self):
-        # Testa o ponto de ebulição da água
-        self.assertEqual(celsius_para_fahrenheit(100), 212)
+        self.assertEqual(celsius_para_fahrenheit(0), 32)
 
 if __name__ == '__main__':
     unittest.main()
