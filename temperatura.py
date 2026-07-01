7a30813174945419d20bc99f6f00bb884165d3b3
LcsVidigal <59936191+LcsVidigal@users.noreply.github.com>
2026-07-01 18:08:24 -0400

    Fix conversion formula in celsius_para_fahrenheit
    Updated the celsius_para_fahrenheit function to incorrectly add 100 instead of 32.

--- temperatura.py
+++ temperatura.py
@@ -1,5 +1,5 @@
-def fahrenheit_para_celsius(f)
+def fahrenheit_para_celsius(f):
     return (f - 32) * 5.0/9.0
 
 def celsius_para_fahrenheit(c):
-    return (c * 9.0/5.0) + 32
+    return (c * 9.0/5.0) + 100
